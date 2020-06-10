353c9df0c2a7bf8e3ffb22d9f175b3c02be4fd38
zzmingTop <1020030139@qq.com>
2020-06-11 00:14:05 +0800

    多个网站同时拉取数据

--- src/main/java/com/royal/recreation/task/Game12Schedule.java
+++ src/main/java/com/royal/recreation/task/Game12Schedule.java
@@ -20,6 +20,8 @@ import org.springframework.util.StringUtils;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -62,50 +64,57 @@ public class Game12Schedule implements InitializingBean {
         if (!getGameType().inAction(actionTime)) {
             return true;
         }
-        String url = "https://api.api861861.com/CQShiCai/getBaseCQShiCaiList.do?lotCode=10010";
-        logger.info("开始爬取数据:{}", url);
         boolean successFlag = false;
-        String result = HttpUtil.httpGet(url);
-        try {
-            if (StringUtils.isEmpty(result)) {
-                return false;
-            }
-            JSONObject json = JSON.parseObject(result);
-            JSONArray dataArr = json.getJSONObject("result").getJSONArray("data");
-            for (Object obj : dataArr) {
-                JSONObject data = (JSONObject) obj;
-                LocalDateTime endTime = LocalDateTime.parse(data.getString("preDrawTime"), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")).plusSeconds(20);
-                Long actionNo = data.getLong("preDrawIssue");
-                String code = data.getString("preDrawCode");
-
-                if (actionTime != null && Duration.between(endTime, actionTime).getSeconds() < 5 * 60) {
-                    successFlag = true;
+        List<String> urls = Arrays.asList("https://api.api861861.com/CQShiCai/getBaseCQShiCaiList.do?lotCode=10010",
+                "https://www.369kj.com/CQShiCai/getBaseCQShiCaiList.do?lotCode=10010");
+        for (String url : urls) {
+            logger.info("开始爬取数据:{}", url);
+            String result = null;
+            try {
+                result = HttpUtil.httpGet(url);
+                if (StringUtils.isEmpty(result)) {
+                    continue;
                 }
-                AwardInfo oldAwardInfo = Mongo.buildMongo().eq("actionNo", actionNo).eq("typeId", 12).findOne(AwardInfo.class);
-                if (oldAwardInfo == null) {
-                    oldAwardInfo = new AwardInfo();
-                    oldAwardInfo.setTypeId(12);
-                    oldAwardInfo.setActionNo(actionNo);
+                JSONObject json = JSON.parseObject(result);
+                JSONArray dataArr = json.getJSONObject("result").getJSONArray("data");
+                for (Object obj : dataArr) {
+                    JSONObject data = (JSONObject) obj;
+                    LocalDateTime endTime = LocalDateTime.parse(data.getString("preDrawTime"), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")).plusSeconds(20);
+                    Long actionNo = data.getLong("preDrawIssue");
+                    String code = data.getString("preDrawCode");
 
-                    oldAwardInfo.setCode(code);
-                    oldAwardInfo.setEndTime(endTime);
-                    oldAwardInfo.setStartTime(endTime.minusMinutes(5));
-                    oldAwardInfo.setAction(false);
-                    Mongo.buildMongo().insert(oldAwardInfo);
-                    logger.info("成功获得中奖信息{}=={}", oldAwardInfo.getActionNo(), oldAwardInfo.getCode());
-                    kjService.action(12);
-                } else {
-                    break;
+                    if (actionTime != null && Duration.between(endTime, actionTime).getSeconds() < 5 * 60) {
+                        successFlag = true;
+                    }
+                    AwardInfo oldAwardInfo = Mongo.buildMongo().eq("actionNo", actionNo).eq("typeId", 12).findOne(AwardInfo.class);
+                    if (oldAwardInfo == null) {
+                        oldAwardInfo = new AwardInfo();
+                        oldAwardInfo.setTypeId(12);
+                        oldAwardInfo.setActionNo(actionNo);
+
+                        oldAwardInfo.setCode(code);
+                        oldAwardInfo.setEndTime(endTime);
+                        oldAwardInfo.setStartTime(endTime.minusMinutes(5));
+                        oldAwardInfo.setAction(false);
+                        Mongo.buildMongo().insert(oldAwardInfo);
+                        logger.info("成功获得中奖信息{}=={}", oldAwardInfo.getActionNo(), oldAwardInfo.getCode());
+                        kjService.action(12);
+                    } else {
+                        break;
+                    }
                 }
+            } catch (Exception e) {
+                logger.info("数据:{}", result);
+                logger.info("action12出现异常", e);
+                continue;
+            } finally {
+                logger.info("执行结果:{}", successFlag);
             }
-        } catch (Exception e) {
-            logger.info("数据:{}", result);
-            successFlag = false;
-            logger.info("action12出现异常", e);
-        } finally {
-            logger.info("执行结果:{}", successFlag);
+            if (successFlag) {
+                return true;
+            }
         }
-        return successFlag;
+        return false;
     }
 
     public GameType getGameType() {
