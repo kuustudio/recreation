d7f0a2f2ac3763562f572f0f3c95b961c572f5ec
zzmingTop <1020030139@qq.com>
2020-07-30 20:56:14 +0800

    新增爬虫

--- src/main/java/com/royal/recreation/task/Game12Schedule.java
+++ src/main/java/com/royal/recreation/task/Game12Schedule.java
@@ -24,7 +24,10 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * 抓取欧洲幸运5数据
@@ -66,7 +69,55 @@ public class Game12Schedule implements InitializingBean {
         if (!getGameType().inAction(actionTime)) {
             return true;
         }
-        return action1(actionTime) || action2(actionTime);
+        return action0(actionTime) || action1(actionTime) || action2(actionTime);
+    }
+
+    private boolean action0(LocalDateTime actionTime) {
+        boolean successFlag = false;
+        String url = "https://www.auluckylottery.com/results/lucky-ball-5";
+        logger.info("开始爬取数据:{}", url);
+        String result = null;
+        try {
+            result = HttpUtil.httpGet(url).replaceAll("[\n\t\r]", "");
+            if (StringUtils.isEmpty(result)) {
+                return false;
+            }
+            String patternStr = "<div class=\"past_numbers\" id=\"\"><div class=\"pn_font1\">(.{26}) \\(ACST\\)  &nbsp;&nbsp;Draw:  <strong>(.{8})</strong></div><div class=\"brt2_ball p_number_ball\"><div class=\"back_red\">(.)</div><div class=\"back_red\">(.)</div><div class=\"back_red\">(.)</div><div class=\"back_red\">(.)</div><div class=\"back_red\">(.)</div></div></div>";
+            Pattern pattern = Pattern.compile(patternStr);
+            Matcher matcher = pattern.matcher(result);
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("EEE, MMM dd, yyyy hh:mm a", Locale.ENGLISH);
+            while (matcher.find()) {
+                LocalDateTime endTime = LocalDateTime.parse(matcher.group(1).replace("am", "AM").replace("pm", "PM"), dateTimeFormatter).minusMinutes(90);
+                Long actionNo = Long.valueOf(matcher.group(2));
+                String code = String.format("%s,%s,%s,%s,%s", matcher.group(3), matcher.group(4), matcher.group(5), matcher.group(6), matcher.group(7));
+
+                if (actionTime != null && Duration.between(endTime, actionTime).getSeconds() < 5 * 60 && !successFlag) {
+                    successFlag = true;
+                }
+                AwardInfo oldAwardInfo = Mongo.buildMongo().eq("actionNo", actionNo).eq("typeId", 12).findOne(AwardInfo.class);
+                if (oldAwardInfo == null && successFlag) {
+                    oldAwardInfo = new AwardInfo();
+                    oldAwardInfo.setTypeId(12);
+                    oldAwardInfo.setActionNo(actionNo);
+
+                    oldAwardInfo.setCode(code);
+                    oldAwardInfo.setEndTime(endTime);
+                    oldAwardInfo.setStartTime(endTime.minusMinutes(5));
+                    oldAwardInfo.setAction(false);
+                    Mongo.buildMongo().insert(oldAwardInfo);
+                    logger.info("成功获得中奖信息{}=={}", oldAwardInfo.getActionNo(), oldAwardInfo.getCode());
+                    kjService.action(12);
+                } else {
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            logger.info("数据:{}", result);
+            logger.info("action12出现异常", e);
+        } finally {
+            logger.info("执行结果:{}", successFlag);
+        }
+        return successFlag;
     }
 
     private boolean action1(LocalDateTime actionTime) {
@@ -137,7 +188,7 @@ public class Game12Schedule implements InitializingBean {
                     Long actionNo = data.getLong("preDrawIssue");
                     String code = data.getString("preDrawCode");
 
-                    if (actionTime != null && Duration.between(endTime, actionTime).getSeconds() < 5 * 60  && !successFlag) {
+                    if (actionTime != null && Duration.between(endTime, actionTime).getSeconds() < 5 * 60 && !successFlag) {
                         successFlag = true;
                     }
                     AwardInfo oldAwardInfo = Mongo.buildMongo().eq("actionNo", actionNo).eq("typeId", 12).findOne(AwardInfo.class);
@@ -178,5 +229,21 @@ public class Game12Schedule implements InitializingBean {
     @Override
     public void afterPropertiesSet() {
         action12List(null);
+    }
+
+
+    public static void main(String[] args) {
+        String str = HttpUtil.httpGet("https://www.auluckylottery.com/results/lucky-ball-5").replaceAll("[\n\t\r]", "");
+        // <div class="past_numbers more_result" id=""><div class="pn_font1">Thu, Jul 30, 2020 08:19 pm (ACST)  &nbsp;&nbsp;Draw:  <strong>50706347</strong></div><div class="brt2_ball p_number_ball"><div class="back_red">8</div><div class="back_red">4</div><div class="back_red">4</div><div class="back_red">4</div><div class="back_red">0</div></div></div>
+        String patternStr =
+                "<div class=\"past_numbers\" id=\"\"><div class=\"pn_font1\">(.{26}) \\(ACST\\)  &nbsp;&nbsp;Draw:  <strong>(.{8})</strong></div><div class=\"brt2_ball p_number_ball\"><div class=\"back_red\">(.)</div><div class=\"back_red\">(.)</div><div class=\"back_red\">(.)</div><div class=\"back_red\">(.)</div><div class=\"back_red\">(.)</div></div></div>";
+        Pattern pattern = Pattern.compile(patternStr);
+        Matcher matcher = pattern.matcher(str);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("EEE, MMM dd, yyyy hh:mm a", Locale.ENGLISH);
+        while (matcher.find()) {
+            System.out.println(LocalDateTime.parse(matcher.group(1).replace("am", "AM").replace("pm", "PM"), dateTimeFormatter).minusMinutes(90));
+            System.out.println(matcher.group(2));
+            System.out.println(matcher.group(3) + matcher.group(4) +matcher.group(5)+matcher.group(6)+matcher.group(7));
+        }
     }
 }
